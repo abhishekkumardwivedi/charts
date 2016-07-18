fdc61e14a1433171e673fccdcaddde6b0d695519
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2016-07-19 01:58:01 +0530

    showing average in graph

--- orders.php
+++ orders.php
@@ -7,24 +7,30 @@ if (!$con) {
 
 mysql_select_db("restoclouddb", $con);
 
-$sth = mysql_query("SELECT revenue FROM projections_sample");
+$sth = mysql_query("SELECT table_no FROM past");
 $rows = array();
-$rows['name'] = 'Revenue';
-while($r = mysql_fetch_array($sth)) {
-    $rows['data'][] = $r['revenue'];
-}
+$rows['name'] = 'Average';
 
-$sth = mysql_query("SELECT overhead FROM projections_sample");
-$rows1 = array();
-$rows1['name'] = 'Overhead';
-while($rr = mysql_fetch_assoc($sth)) {
-    $rows1['data'][] = $rr['overhead'];
+// Average time taken on each table
+while($r = mysql_fetch_array($sth)) {
+    $sum = 0;
+    $count = 0;
+    
+    $sth1 = mysql_query("SELECT time_to_sec(timediff(ack, req)) FROM orders where table_no=" . $r['table_no']);
+    while($rr = mysql_fetch_assoc($sth1)) {
+        $sum = $sum + $rr['time_to_sec(timediff(ack, req))'];
+        $count = $count + 1;
+    }
+    if($count == 0) {
+        $rows['data'][] = 0;
+    } else {
+        $rows['data'][] = ($sum/$count);
+    }
 }
 
 $result = array();
 array_push($result,$rows);
-array_push($result,$rows1);
-
+//array_push($result,$rows1);
 
 print json_encode($result, JSON_NUMERIC_CHECK);
 
